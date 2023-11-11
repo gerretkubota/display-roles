6798867bf62c9bd7c9f1793c18725f7852cfbd4c
gerretkubota <shy@Shys-MacBook-Pro.local>
2023-11-11 14:24:08 -0800

    writing prompt inside of DisplayRoles

--- src/DisplayRoles/DisplayRoles.ts
+++ src/DisplayRoles/DisplayRoles.ts
@@ -1,3 +1,16 @@
+/* 
+  1. Display a step-by-step component that represents four distinct user roles:
+  - No Role
+  - User
+  - Moderator
+  - Admin
+
+  2. If the user exists - Automatically set the step (user role) according to the fetched data.
+  3. If you're creating a new user - Allow the user to manually select a step (user role) by clicking on it
+  4. Display only a checkmark inside the circle of the selected step.
+  5. Display the label (role name) below each step circle
+*/
+
 const DisplayRoles = () => {
 
 };
